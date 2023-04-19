2b3fa8133d34aa91c600018b8c14c4e1f8cf7d11
Brian-Weloba <bweloba@gmail.com>
2023-04-19 07:45:25 +0000

    Add error handling

---
+++ src/main/java/tech/saturdev/pakakumi/controller/CustomErrorController.java
@@ -0,0 +1,43 @@
+package tech.saturdev.pakakumi.controller;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.ServletWebRequest;
+
+@ControllerAdvice
+@RequestMapping("/error")
+public class CustomErrorController implements ErrorController {
+
+    private ErrorAttributes errorAttributes;
+
+    public CustomErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    @RequestMapping(produces = "text/html")
+    public String handleError(HttpServletRequest request, Model model) {
+        Map<String, Object> errorAttributes = this.errorAttributes.getErrorAttributes(new ServletWebRequest(request),
+                ErrorAttributeOptions.of(ErrorAttributeOptions.Include.MESSAGE,
+                        ErrorAttributeOptions.Include.STACK_TRACE,
+                        ErrorAttributeOptions.Include.EXCEPTION,
+                        ErrorAttributeOptions.Include.BINDING_ERRORS));
+
+        model.addAttribute("status", errorAttributes.get("status"));
+        model.addAttribute("error", errorAttributes.get("error"));
+        model.addAttribute("message", errorAttributes.get("message"));
+        model.addAttribute("path", errorAttributes.get("path"));
+
+        return "error";
+    }
+
+}
